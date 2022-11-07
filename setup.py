80dbdfec4cfb0ee9f179d0427b60a0379c2bfb60
Leonardo S.B. Lang <49520669+leonardosblang@users.noreply.github.com>
2022-11-06 22:30:22 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"],
+                     "build_exe": "build"}
+BASE_PLATFORM = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name="build",
+    version="1.0",
+    description="Mocktests",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=None,
+                            target_name="test-build.exe")],
+)
